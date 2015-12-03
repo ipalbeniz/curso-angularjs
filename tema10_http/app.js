d86828491258860c680ba0b80d440a3e89a5a5db
Iñaki Pérez de Albéniz <ipalbeniz@gmail.com>
2015-12-03 18:18:53 +0100

    Añadir ejercicio del tema 10

---
+++ tema10_http/app.js
@@ -0,0 +1,33 @@
+angular.module('app', ['angular-loading-bar', 'uiGmapgoogle-maps', 'google.places'])
+
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
+
+.controller('WeatherController', function($scope, $http) {
+    $scope.autocompleteOptions = {
+        componentRestrictions: {
+            country: 'es'
+        },
+        types: ['geocode']
+    };
+
+    $scope.getWeather = function() {
+        $http.get("http://api.openweathermap.org/data/2.5/weather?q=" + $scope.cityName.address_components[0].long_name + ",es&units=metric&lang=es&APPID=4699be61f7412c1d200eeb9d559b0408")
+            .success(function(data) {
+                $scope.weather = data;
+                $scope.iconUrl = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+                $scope.map = {
+                    center: {
+                        latitude: data.coord.lat,
+                        longitude: data.coord.lon
+                    },
+                    zoom: 12
+                };
+            });
+    };
+});
